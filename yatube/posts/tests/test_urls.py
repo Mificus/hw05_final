07b8b234949f3dea42735a237963de98abf445f6
Максим Ласточкин <mifswallow@mail.ru>
2022-11-25 15:47:21 +0300

    Second commit

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,9 +1,7 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.core.cache import cache
 from django.test import TestCase, Client
-from django.urls import reverse
 
 from ..models import Post, Group
 
